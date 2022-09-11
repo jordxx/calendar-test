b3b86bd73669d20dd12c03657612dafe1e2e6890
jordxx <danieljordan553@gmail.com>
2022-09-11 15:56:51 +0700

    feat: CRUD fullcalendar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import FullCalendar from '@fullcalendar/react' // must go before plugins
-import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
-import './App.css';
 import { useEffect, useState } from 'react';
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import interactionPlugin from '@fullcalendar/interaction'
+import './App.css';
+import { Button, Label, Modal, TextInput } from 'flowbite-react';
 
 function App() {
   const [event, setEvent] = useState([])
+  const [detail, setDetail] = useState({})
+  // SHOW MODAL
+  const [show, setShow] = useState(false)
+  const [showDetail, setShowDetail] = useState(false)
+  // DUE DATE CALENDAR
+  const [dueDate, setDueDate] = useState("")
   const [input, setInput] = useState({
     title: "",
     date: "",
     color: ""
   })
-  
   // FETCH DATA
   useEffect(() => {
     fetch(`http://localhost:4000/Events`, {
@@ -24,22 +31,37 @@ function App() {
       })
       .then((data) => setEvent(data))
       .catch((err) => console.log(err))
+
   }, [])
 
   // TO HANDLE DELETE EVENTS
   const eventClick = (event) => {
-    console.log(event.event._def.publicId)
+    setShowDetail(true)
+    if (event.event._def.publicId) {
+      fetch(`http://localhost:4000/Events/${event.event._def.publicId}`)
+        .then((response) => {
+          if (!response.ok) throw new Error("Fetch error")
+          return response.json()
+        })
+        .then((data) => {
+          setDetail(data)
+          setInput({
+            title: data.title,
+            date: data.date,
+            color: data.color
+          })
+        })
+        .catch((err) => console.log(err))
+    }
   }
-
   const handleChange = (event) => {
     const { name, value } = event.target
     setInput({ ...input, [name]: value })
   }
-
   // INPUT DATA TO SERVER
   const handleSubmit = (event) => {
     event.preventDefault()
-    const { title, date } = input
+    const { title } = input
     fetch(`http://localhost:4000/Events`, {
       method: 'post',
       headers: {
@@ -47,55 +69,170 @@ function App() {
       },
       body: JSON.stringify({
         title: title,
-        date: date,
+        date: dueDate,
         color: 'red'
       })
     })
       .then((res) => {
-        if (!res.ok) return res.json()
-          .then((message) => { throw message })
+        if (!res.ok) throw new Error("Create failed")
         return res.json()
       })
-      .then((data) => console.log(data))
+      .then((data) => setEvent(data))
+      .finally(() => setShow(false))
+      .catch((err) => console.log(err))
+  }
+  const addTask = (selectInfo) => {
+    setShow(true)
+    setDueDate(selectInfo.startStr)
+  }
+  const onClose = () => {
+    setShow(false)
+  }
+  const onCloseEvent = () => {
+    setShowDetail(false)
+  }
+  const handleUpdate = (event) => {
+    event.preventDefault()
+    const { title, date, color } = input
+    fetch(`http://localhost:4000/Events/${detail.id}`, {
+      method: 'put',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        title: title,
+        date: date,
+        color: color
+      })
+    })
+      .then((res) => {
+        if (!res.ok) throw new Error("Update failed")
+        return res.json()
+      })
+      .finally(() => setShowDetail(false))
+      .catch((err) => console.log(err))
+
+  }
+  const handleDelete = (_id) => {
+    fetch(`http://localhost:4000/Events/${_id}`, {
+      method: 'delete',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then((response) => {
+        if (!response.ok) throw new Error("Fetch error")
+        return response.json()
+      })
+      .then(() => setShowDetail(false))
+      .catch((err) => console.log(err))
   }
   return (
     <div className="App">
-      <h1>ADD EVENTS</h1>
-      <form onSubmit={handleSubmit}>
-        <label
-          htmlFor='formTitle'
-        >Event: </label>
-        <input
-          type='text'
-          className='border'
-          name='title'
-          value={input.title}
-          placeholder='name'
-          onChange={handleChange}
-        />
+      {/* ADD EVENT */}
+      <Modal
+        show={show}
+        size="md"
+        popup={true}
+        onClose={onClose}
+      >
+        <Modal.Header />
+        <Modal.Body>
+          <form onSubmit={handleSubmit}>
+            <Label
+              htmlFor='formTitle'
+            >Event: </Label>
+            <TextInput
+              type='text'
+              className='border'
+              name='title'
+              value={input.title}
+              placeholder='name'
+              onChange={handleChange}
+            />
+            <div className='mt-4 rounded-xl'>
+              <Button onClick={handleSubmit} className='border text-white text-sm'>SUBMIT</Button>
 
-        <label
-          htmlFor='formDate'
-        >Date: </label>
-        <input
-          type='date'
-          className='border'
-          name='date'
-          value={input.date}
-          placeholder='date'
-          onChange={handleChange}
-        />
-        <button className='border bg-blue-600 text-white'>SUBMIT</button>
-      </form>
+            </div>
+          </form>
+        </Modal.Body>
+      </Modal>
+
+      {/* UPDATE / DELETE EVENT */}
+      <Modal
+        show={showDetail}
+        size="xl"
+        popup={true}
+        onClose={onCloseEvent}
+      >
+        <Modal.Header />
+        <Modal.Body>
+          <div className='container m-auto'>
+            <h1 className='text-center'>EVENT</h1>
+            <div>
+              <p>{input.title}</p>
+              <p>{input.date}</p>
+              <form onSubmit={handleUpdate}>
+                <Label
+                  htmlFor='updateTitle'
+                >Event: </Label>
+                <TextInput
+                  type='text'
+                  className='border'
+                  name='title'
+                  value={input.title}
+                  onChange={handleChange}
+                />
+
+                <Label
+                  htmlFor='updateDate'
+                >Date: </Label>
+                <TextInput
+                  type='date'
+                  className='border'
+                  name='date'
+                  value={input.date}
+                  onChange={handleChange}
+                />
+
+                <Label
+                  htmlFor='updateColor'
+                >Color: </Label>
+                <TextInput
+                  type='text'
+                  className='border'
+                  name='color'
+                  value={input.color}
+                  onChange={handleChange}
+                />
+
+                <div>
+                  <button onClick={handleUpdate} type="button" className="text-white bg-gradient-to-r from-blue-500 
+              via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none
+               focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 
+               py-2.5 text-center mr-2 mb-2">Update</button>
+
+                  <button onClick={() => handleDelete(detail.id)} type="button" className="text-white bg-gradient-to-r from-red-400 
+            via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none 
+            focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 
+            text-center mr-2 mb-2">Delete</button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </Modal.Body>
+      </Modal>
       <div className='container m-auto w-1/2'>
         <FullCalendar
-          plugins={[dayGridPlugin]}
-          initialView="dayGridMonth"
+          plugins={[dayGridPlugin, interactionPlugin]}
           editable={true}
           selectable={true}
           selectMirror={true}
+          droppable={true}
+          initialView="dayGridMonth"
           events={event}
-          eventClick={eventClick}
+          eventClick={eventClick} //FOR DELETE
+          select={addTask}
         />
       </div>
     </div>
